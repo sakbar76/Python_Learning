1288edcdde83cd17d184074fd943d1f3c671179a
Shakeel.Akbar <shakeel.akbar@emaratech.ae>
2020-03-03 16:06:50 +0400

    For and While loop with break keyword for terminating the loop including conditional statements as well.

--- python_learning_howtothink/python_learning/ForLoopExample.py
+++ python_learning_howtothink/python_learning/ForLoopExample.py
@@ -1,28 +1,45 @@
 
 # For loop example with list/array of strings
+print ("Loop Example 1")
 for friend in ["Joe", "Zoe", "Zuki", "Thandi", "Paris"]:
     invite = "Hi " + friend + ". Please come to my party!"
     print(invite)
 
 
 # simple for loop example 2
+print ("Loop Example 2")
 for i in [0,1,2,3,4]:
     print("value of i: ",i)
 
 
 # using range keyword in for loop
+print ("Loop Example 3")
 for i in range (4):
     print("value of i using range keyaowrd: ",i)
 
 
 # for case where variable not used in loop
+print ("Loop Example 4")
 for _ in range(5):
     print("This is loop without variable")
 
 
 # for loop example 5 with instance of list/object
+print ("Loop Example 5")
 numbers = [5, 6, 32, 21, 9]
 running_total = 0
 for number in numbers:
     running_total = running_total + number
     print(running_total)
+
+# for loop example 6
+print ("Loop Example 6")
+for x in range(13):
+    print(x, "\t", 2**x)
+
+
+# for loop example 7
+print ("Loop Example 7")
+for x in range (1,7):
+    print(2 * x, end=" ")
+print ()
--- python_learning_howtothink/python_learning/WhileLoopExample.py
+++ python_learning_howtothink/python_learning/WhileLoopExample.py
@@ -1,3 +1,43 @@
 
 # While Loop Example
+print("While Loop # 1 Start")
+n=6
+current_sum=0
+i=0
 
+while i<=n:
+    current_sum+=i
+    i+=1
+
+    # if i==7:
+    #     break
+
+    print("Value of i: ",i)
+    print ("Current_Sum: ",current_sum)
+
+print ("End of While Loop # 1")
+
+
+# while loop examples # 2
+
+print ("Start of While Loop # 2")
+n = 1027371
+while n != 1:
+    print(n, end=", ")
+    if n % 2 == 0: # n is even
+        n = n // 2
+    else: # n is odd
+        n = n * 3 + 1
+
+print(n, end=".\n")
+print("End of While Loop # 2")
+
+# while loop with while boolean expression example
+print ("While Loop Example # 3")
+total = 0
+while True:
+    response = input("Enter the next number. (Leave blank to end)")
+    if response == "" or response == "-1":
+        break
+    total += int(response)
+    print("The total of the numbers you entered is ", total)
